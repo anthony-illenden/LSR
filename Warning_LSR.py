b8656a0a28bbb6d5f80006f9541b97777efcb600
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2023-07-09 12:52:29 -0400

    Update and rename EXPERIMENTALwarnings.py to Warning_LSR.py

--- Warning_LSR.py
+++ Warning_LSR.py
@@ -3,7 +3,9 @@ import requests
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
-from shapely.geometry import MultiPolygon, Polygon
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Polygon as mplPolygon
+from cartopy.mpl.geoaxes import GeoAxes
 import pandas as pd
 from metpy.plots import USCOUNTIES
 import cartopy.crs as ccrs
@@ -11,24 +13,17 @@ import cartopy. feature as cfeature
 import matplotlib.pyplot as plt
 import datetime
 
-url = 'https://www.spc.noaa.gov/climo/reports/230615_rpts_torn.csv'
+tor_url = 'https://www.spc.noaa.gov/climo/reports/230615_rpts_torn.csv'
 
-df_tor = pd.read_csv(url)
+df_tor = pd.read_csv(tor_url)
 
-url = 'https://www.spc.noaa.gov/climo/reports/230615_rpts_hail.csv'
+hail_url = 'https://www.spc.noaa.gov/climo/reports/230615_rpts_hail.csv'
 
-df_hail = pd.read_csv(url)
+df_hail = pd.read_csv(hail_url)
 
-url = 'https://www.spc.noaa.gov/climo/reports/230615_rpts_wind.csv'
+wind_url = 'https://www.spc.noaa.gov/climo/reports/230615_rpts_wind.csv'
 
-df_wind = pd.read_csv(url)
-
-mapcrs = ccrs.LambertConformal(central_longitude=-85.6, central_latitude=44.3, standard_parallels=(30, 60)) 
-datacrs = ccrs.PlateCarree()
-proj = ccrs.Stereographic(central_longitude=-85, central_latitude=40)
-
-now = datetime.datetime.utcnow()
-now = now.strftime("%m/%d/%Y %H:%M")
+df_wind = pd.read_csv(wind_url)
 
 for i in range(len(df_wind['Speed'])):
     value = df_wind['Speed'][i]
@@ -37,8 +32,8 @@ for i in range(len(df_wind['Speed'])):
             df_wind['Speed'][i] = float(value)
         except ValueError:
             df_wind['Speed'][i] = np.nan
-        
-url = 'https://mesonet.agron.iastate.edu/api/1/cow.json?wfo=DTX&begints=2023-06-25T12:00:00Z&endts=2023-06-26T12:00:00Z&hailsize=1&wind=58&phenomena=TO&phenomena=SV&phenomena=MA&phenomena=FF&phenomena=DS&lsrbuffer=15&warningbuffer=1'
+
+url = 'https://mesonet.agron.iastate.edu/api/1/cow.json?wfo=DTX&begints=2023-06-15T12:00:00Z&endts=2023-06-16T12:00:00Z&hailsize=1&wind=58&phenomena=TO&phenomena=SV&phenomena=MA&phenomena=FF&phenomena=DS&lsrbuffer=15&warningbuffer=1'
 response = requests.get(url)
 data = response.json()
 
@@ -79,66 +74,71 @@ df = pd.DataFrame(warning_data, columns=['id', 'year', 'wfo', 'phenomena', 'even
                                          'stormreports_all', 'verify', 'lead0', 'areaverify', 'sharedborder',
                                          'geometry'])
 
-mapcrs = ccrs.LambertConformal(central_longitude=-85.6, central_latitude=44.3, standard_parallels=(30, 60))
-datacrs = ccrs.PlateCarree()
-
 fig = plt.figure(figsize=(16, 12))
-ax = fig.add_subplot(1, 1, 1, projection=mapcrs)
-ax.set_extent([-85.5, -82, 41.5, 44.0], datacrs)
+ax = fig.add_subplot(1, 1, 1, projection=ccrs.LambertConformal(central_longitude=-85.6, central_latitude=44.3, standard_parallels=(30, 60)))
+ax.set_extent([-85.5, -82, 41.5, 44.0])
+
 ax.add_feature(cfeature.COASTLINE.with_scale('50m'))
 ax.add_feature(cfeature.STATES.with_scale('50m'), alpha=.75)
 ax.add_feature(cfeature.BORDERS.with_scale('50m'), linestyle=':', linewidth=0.5)
 ax.add_feature(USCOUNTIES.with_scale('5m'), linewidth=0.25, alpha = 0.5)
 
+for i in range(len(df_wind['Speed'])):
+    value = df_wind['Speed'][i]
+    if value != 'UNK':
+        try:
+            df_wind['Speed'][i] = float(value)
+        except ValueError:
+            df_wind['Speed'][i] = np.nan
+        
 for index, row in df_hail.iterrows():
     if row['Size'] >= 200:
-        ax.scatter(row['Lon'], row['Lat'],transform=datacrs, marker='^', c='black', alpha=0.7, s = 150, label = 'Large Hail Report (2"+)')
+        ax.scatter(row['Lon'], row['Lat'],transform=ccrs.PlateCarree(), marker='^', c='black', alpha=0.7, s = 150,zorder=2, label = 'Large Hail Report (2"+)')
     else:
-        ax.scatter(row['Lon'], row['Lat'],transform=datacrs, c='green', alpha=0.7, s=150, label='Hail Report')
+        ax.scatter(row['Lon'], row['Lat'],transform=ccrs.PlateCarree(), c='green', alpha=0.7, s=150,zorder=2, label='Hail Report')
 
 for index, row in df_wind.iterrows():
     if (row['Speed'] != 'UNK' and row['Speed'] > 65):
-        ax.scatter(row['Lon'], row['Lat'],transform=datacrs, marker = 's', c='black', alpha=0.7, s=150, label='High Wind Report (65KT+)')
+        ax.scatter(row['Lon'], row['Lat'],transform=ccrs.PlateCarree(), marker = 's', c='black', alpha=0.7, s=150,zorder=2, label='High Wind Report (65KT+)')
     else:
-        ax.scatter(row['Lon'], row['Lat'],transform=datacrs, c='blue', alpha=0.7, s=150, label='Wind Report')
+        ax.scatter(row['Lon'], row['Lat'],transform=ccrs.PlateCarree(), c='blue', alpha=0.7, s=150,zorder=2, label='Wind Report')
 
-
-ax.scatter(df_tor['Lon'].astype(float), df_tor['Lat'].astype(float), transform=datacrs, c='red', label='Tornado Reports', s = 150)
+ax.scatter(df_tor['Lon'].astype(float), df_tor['Lat'].astype(float), transform=ccrs.PlateCarree(), c='red', label='Tornado Reports', s = 150, zorder=2)
 
 handles, labels = ax.get_legend_handles_labels()
 unique_labels = list(set(labels))
 unique_handles = [handles[labels.index(label)] for label in unique_labels]
 ax.legend(unique_handles, unique_labels)
 
-
 colors = {'SV': 'yellow', 'TO': 'red', 'FF': 'green', 'MA': 'orange', 'DS': 'brown'}
+
+patches = []
+phenomena_colors = []
 
 for index, row in df.iterrows():
     geometry = row['geometry']
     if geometry:
         if geometry['type'] == 'Polygon':
             coordinates = geometry['coordinates']
-            if isinstance(coordinates[0][0], float): 
+            if isinstance(coordinates[0][0], float):
                 coordinates = [coordinates]
-            polygon = Polygon(coordinates[0])
-            phenomena = row['phenomena']
-            color = colors.get(phenomena)
-            if color:
-                ax.add_patch(Polygon(polygon.exterior, fc=color, ec='black', lw=1, transform=datacrs))
+            for polygon_coords in coordinates:
+                polygon = mplPolygon(polygon_coords, closed=True)
+                patches.append(polygon)
+                phenomena_colors.append(colors.get(row['phenomena'], 'gray'))
+                ax.text(polygon_coords[0][0], polygon_coords[0][1], str(row['eventid']), transform=ccrs.PlateCarree(), fontsize=8)
         elif geometry['type'] == 'MultiPolygon':
             polygons = geometry['coordinates']
-            valid_polygons = []
-            for coords in polygons:
-                if coords and isinstance(coords[0][0], float): 
-                    valid_polygons.append(coords)
-            if not valid_polygons:
-                continue
-            multipolygon = MultiPolygon(valid_polygons)
-            phenomena = row['phenomena']
-            color = colors.get(phenomena)
-            if color:
-                for polygon in multipolygon:
-                    ax.add_patch(Polygon(polygon.exterior, fc=color, ec='black', lw=1, transform=datacrs))
+            for multipolygon_coords in polygons:
+                for polygon_coords in multipolygon_coords:
+                    polygon = mplPolygon(polygon_coords, closed=True)
+                    patches.append(polygon)
+                    phenomena_colors.append(colors.get(row['phenomena'], 'gray'))
+                    ax.text(polygon_coords[0][0], polygon_coords[0][1], str(row['eventid']), transform=ccrs.PlateCarree(), fontsize=8)
 
-#plt.title('SPC LSR valid for ' + str(now) +' UTC')
+collection = PatchCollection(patches, facecolor=phenomena_colors, edgecolor='black', lw=1, transform=ccrs.PlateCarree())
+ax.add_collection(collection)
+
+plt.title('SPC LSR')
+
 plt.show()
